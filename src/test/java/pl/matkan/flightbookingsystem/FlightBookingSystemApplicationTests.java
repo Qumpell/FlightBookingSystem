dc935a8e1d3eda20c09072b5e36df6bb395426ad
Mateusz Kantorski <qqumpel@gmail.com>
2025-04-13 20:13:51 +0200

    first commit

---
+++ src/test/java/pl/matkan/flightbookingsystem/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matkan.flightbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
