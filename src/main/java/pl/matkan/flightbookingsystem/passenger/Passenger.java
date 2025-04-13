f7636946cb9f34741182cd9501073568838ccd21
Mateusz Kantorski <qqumpel@gmail.com>
2025-04-13 21:45:56 +0200

    Add validation for passenger entity and dto

--- src/main/java/pl/matkan/flightbookingsystem/passenger/Passenger.java
+++ src/main/java/pl/matkan/flightbookingsystem/passenger/Passenger.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,8 +23,23 @@ public class Passenger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Firstname is required")
+    @Size(min = 2, max = 50, message = "Firstname must be between 2 and 50 characters")
     private String firstname;
+
+    @NotBlank(message = "Lastname is required")
+    @Size(min = 2, max = 50, message = "Lastname must be between 2 and 50 characters")
     private String lastname;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^\\+\\d{9,15}$",
+            message = "Invalid phone number format"
+    )
     private String phone;
 }
--- src/main/java/pl/matkan/flightbookingsystem/passenger/PassengerRequest.java
+++ src/main/java/pl/matkan/flightbookingsystem/passenger/PassengerRequest.java
@@ -1,9 +1,29 @@
 package pl.matkan.flightbookingsystem.passenger;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record PassengerRequest(
+
+        @NotBlank(message = "Firstname is required")
+        @Size(min = 2, max = 50, message = "Firstname must be between 2 and 50 characters")
         String firstname,
+
+        @NotBlank(message = "Lastname is required")
+        @Size(min = 2, max = 50, message = "Lastname must be between 2 and 50 characters")
         String lastname,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
         String email,
+
+        @NotBlank(message = "Phone number is required")
+        @Pattern(
+                regexp = "^\\+\\d{9,15}$",
+                message = "Invalid phone number format"
+        )
         String phone
 ) {
 }
