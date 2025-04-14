6a863460844fbc66eb0bb0f0be54172e42a83578
Qumpell <qqumpel@gmail.com>
2025-04-14 21:14:29 +0200

    Add relationships between entities

--- src/main/java/pl/matkan/flightbookingsystem/passenger/Passenger.java
+++ src/main/java/pl/matkan/flightbookingsystem/passenger/Passenger.java
@@ -1,9 +1,6 @@
 package pl.matkan.flightbookingsystem.passenger;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -12,8 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
